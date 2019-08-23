d93ac27ca89b2d33bcde5866be1de730a0c9ba29
Jodie A <Zeze@Jodies-MacBook-Pro.local>
2019-08-24 00:50:04 +0300

    python learning

---
+++ main.py
@@ -0,0 +1,52 @@
+#DAY 1 AND DAY 2
+#comment
+#print("Hello,World")
+"""
+multiline comment is here 
+"""
+print("hi")#comment
+#####################
+#DAY 3
+x = 5
+y = "john"
+print(x)
+print(y)
+
+x = 4 #x1 is a type of int 
+x = "sally" #x1 is a type of string
+print(x)
+x = "john"
+#same as
+x = 'john'
+#double quotes is the same as single quotes
+print(x)
+x , y, z = "orange","banana" , "cherry"
+print(x+" "+y+" "+z)
+
+##################
+x = y= z = "orange"
+print(x+" "+y+" "+z)
+##################
+
+x="awesome"
+print("python is "+x)
+
+x = "python is "
+y="awesome"
+z= x+y
+print(z)
+
+##################
+x = 5
+y = 10
+print(x+y)
+##################
+
+x = 5
+y = "john"
+print(x + y)
+
+
+
+
+
