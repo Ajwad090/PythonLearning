388dd8a70208ebc978f27533ca228867cbefac92
Jodie A <Zeze@Jodies-MacBook-Pro.local>
2019-08-30 20:24:55 +0300

    Python Strings

---
+++ Day7.py
@@ -0,0 +1,17 @@
+#Week 2
+#Day 7 :  Python Strings
+print("hello")
+print('hello')
+a = "hello"
+print(a)
+##################################################
+
+print("\n")
+a = """ hi i am learning python , in 100 days """
+
+##################################################
+
+a = "Hello , world"
+print(a[1])
+
+print(a[2:5])
