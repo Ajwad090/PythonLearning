480636b8fbf61f98f32689be630ef19a01982d9b
Jodie A <Zeze@Jodies-MacBook-Pro.local>
2019-08-30 20:02:38 +0300

    Python Casting

---
+++ Day6.py
@@ -0,0 +1,27 @@
+#Week 2
+#Day 6 : Python Casting 
+x = int(1)
+y = int(2.3)
+z = int("3")
+print(x)
+print(y)
+print(z)
+##################
+print("\n")
+x = float(1)
+y = float(2.3)
+z = float("3")
+w = float("4.2")
+print(x)
+print(y)
+print(z)
+print(w)
+##################
+print("\n")
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
+##################
