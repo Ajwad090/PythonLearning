2ec42b4a9839df992af478fa1e4f581cf17215c4
Jodie A <Zeze@Jodies-MacBook-Pro.local>
2019-09-05 19:02:49 +0300

    Python List 2

---
+++ Day14.py
@@ -0,0 +1,15 @@
+#Week 3
+#Day 14 : Python List 2
+alist  = ["A","B","C","D","E"]
+print(alist[1:3])
+alist  = ["Apple","Banana","Cherry"]
+if "Apple" in alist:
+    print("Yes, Apple' is in the fruit list")
+
+alist = ["Python"] * 4
+print(alist)
+
+one = ["john" , "jackson"]
+two = ["judy" , "janett"] 
+
+print(one + two)
