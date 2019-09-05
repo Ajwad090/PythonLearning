1a2a7f74c9a56028a1b0bd08add876773120a8fb
Jodie A <Zeze@Jodies-MacBook-Pro.local>
2019-09-05 18:37:31 +0300

    Python List

---
+++ Day13.py
@@ -0,0 +1,27 @@
+#Week 3
+#Day 13 : Python List
+s = []
+print(s)
+numbers = [1,2,3,4,5]
+print(numbers)
+alist = ["apple" , "banana","cherry"] 
+print(alist)
+alist = ["apple" , "banana","cherry",1,2,3] 
+print(alist[1])
+print(alist)
+alist = [1.3 , 1.1 , 1.2]
+
+
+
+for x in alist:
+    print(x)
+
+alist = ["apple" , "banana","cherry"] 
+alist[1] = "blackcurrant"
+print(alist)
+
+del alist[0]
+print(alist)
+
+del alist
+
